921f5e95b471b04225336703c0156e98e2e77276
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-19 15:13:04 +0300

    MissileTypes Enum added

---
+++ Shayetet6/Shayetet6/MissileTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shayetet6
+{
+    enum MissileTypes
+    {
+        Torpedo,
+        Balistic,
+        Cruise
+    }
+}
