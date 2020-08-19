0f6ad08106375f89e7a77a644471f95f1e2a2f71
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-19 17:40:24 +0300

    Add Missile functionallity added

---
+++ Shayetet6/Shayetet6/MissleFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shayetet6
+{
+    public static class MissleFactory
+    {
+        public static Missile CreateMissile(string missileType)
+        {
+            switch(missileType)
+            {
+                case "Torpedo":
+                    return new Torpedo(missileType, 100 , 1000);
+                case "Cruise":
+                    return new Cruise(missileType, 20 ,3000) ;
+                case "Balistic":
+                    return new Balistic(missileType, 50, 150);
+                default:
+                    return null;
+            }
+        }
+        public static Missile CreateMissile()
+        {
+            string type = UserInputValidator.ReadMissileTypeName();
+            return CreateMissile(type);
+        }
+    }
+}
