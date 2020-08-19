706f1385df908b2765c645278f48722cb717ba52
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-19 15:43:25 +0300

    LauncherHandler and LauncherMenuCreator classes added

---
+++ Shayetet6/Shayetet6/LauncherHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shayetet6
+{
+    class LauncherHandler
+    {
+        public MissileLauncher Launcher { get; private set; }
+        public LauncherHandler(MissileLauncher launcher)
+        {
+            Launcher = launcher;
+        }
+
+    }
+}
---
+++ Shayetet6/Shayetet6/LauncherMenuCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MenuBuilder;
+
+namespace Shayetet6
+{
+    class LauncherMenuCreator
+    {
+        public Menu<int> LauncherMainMenuCreator()
+        {
+
+        }
+    }
+}
