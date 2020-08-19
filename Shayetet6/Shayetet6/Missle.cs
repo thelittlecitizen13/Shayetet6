6041649478ad6bed2c307971b04a9f12a417e5d6
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-19 15:03:46 +0300

    abstract class Missle added

---
+++ Shayetet6/Shayetet6/Missle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shayetet6
+{
+    public abstract class Missle
+    {
+        public string Name { get; protected set; }
+        public bool IsFailed { get; protected set; }
+        public double TechniqueChance { get; protected set; }
+        public void Fail()
+        {
+            IsFailed = true;
+        }
+        public void ChangeTechnique(double chance)
+        {
+            TechniqueChance = chance;
+        }
+
+    }
+}
