72e08a9f16121d44c1a63b23ad91177a4d51e7eb
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-20 08:53:30 +0300

    Technique class added

---
+++ Shayetet6/Shayetet6/Technique.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shayetet6
+{
+    class Technique
+    {
+        public string TargetMissile { get; set; }
+        public int LaunchSuccessChance { get; set; }
+        public Technique(string missileType)
+        {
+            TargetMissile = missileType;
+        }
+    }
+}
